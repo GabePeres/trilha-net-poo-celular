e233986ba86bea040fc8df86afca76f354fa17d5
Gabriel Peres <gabrielperes2009@gmail.com>
2025-01-15 16:42:04 -0300

    Ajustes desafio celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero : "12345", modelo: "modelo 1", imei: "1111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+Console.WriteLine("\nSmartphone Iphone:");
+Smartphone iphone = new Iphone(numero : "54321", modelo: "modelo 2", imei: "2222222222", memoria: 124);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
